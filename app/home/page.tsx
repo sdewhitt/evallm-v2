ad63fbf4ce8ea62828b13a5bbc32086ef31bdf52
sdewhitt <seth_dewhitt@outlook.com>
2025-07-20 21:02:09 -0700

    starting to reformat the home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,40 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import Logout from '@/components/Logout';
 
+
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+
+interface Experiment {
+  prompt: string;
+  expected: string;
+  responsesAndEvaluations: {
+    [model: string]: {
+      response: string;
+      evaluation: {
+        responseTime: number;
+        exactMatch: boolean;
+        similarity: number;
+        bleu: number;
+        rouge: number[];
+        perplexity: number;
+      };
+    };
+  };
+}
+
+const models = ['llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma2-9b-it'];
+const defaultStatistics: { [key: string]: number | string} = {
+  numResponses: 0,
+  avgSimilarity: 0,
+  avgBleu: 0,
+  avgRouge: 0,
+}
+const defaultModelStatistics: { [key: string]: { [key: string]: number | string} } = models.reduce((acc: { [key: string]: { [key: string]: number | string} }, model) => {
+  acc[model] = { ...defaultStatistics };
+  return acc;
+}, {});
+
+
 const Home = async () => {
 
     const session = await auth();
