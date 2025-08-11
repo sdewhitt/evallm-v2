374955b45c812f05aab1c05e0a4b64f40d0d89e0
sdewhitt <seth_dewhitt@outlook.com>
2025-08-10 23:54:55 -0700

    redeploy

--- auth.ts
+++ auth.ts
@@ -20,6 +20,15 @@ export const {
             }
         })
     ],
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
+        }
+    },
     session: {
         strategy: "jwt"
     },
